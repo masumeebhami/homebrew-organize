ac938e9f98ab99a334ac02cc58acb6e378b26e57
Masi <masumeebhami@gmail.com>
2025-06-24 11:45:40 +0200

    add ruby file

---
+++ Formula/organize.rb
@@ -0,0 +1,15 @@
+class Organize < Formula
+  desc "Smart file organizer for macOS, written in Rust"
+  homepage "https://github.com/masumeebhami/organize"
+  url "https://github.com/masumeebhami/organize/releases/download/v0.1.0/organize-macos.tar.gz"
+  sha256 "PASTE_YOUR_SHA_HERE"
+  license "MIT"
+
+  def install
+    bin.install "organize"
+  end
+
+  test do
+    system "#{bin}/organize", "--help"
+  end
+end
