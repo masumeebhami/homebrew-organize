1f08338e89137e6bbe72258063b040e9d64477eb
Masi <masumeebhami@gmail.com>
2025-06-24 18:57:49 +0200

    added new version

--- Formula/organize.rb
+++ Formula/organize.rb
@@ -1,8 +1,8 @@
 class Organize < Formula
   desc "Smart file organizer for macOS, written in Rust"
   homepage "https://github.com/masumeebhami/organize"
-  url "https://github.com/masumeebhami/organize/releases/download/v0.1.1/organize-macos.tar.gz"
-  sha256 "156143165d394940392d23022059e9d200432b7309cb51d793f30726111115aa"
+  url "https://github.com/masumeebhami/organize/releases/download/v0.1.2/organize-macos.tar.gz"
+  sha256 "041e6cc3c37f76b9b5df7a82efc30cc152ba61c5e18bafc9f6c377fb09be455c"
   version "v0.1.1"
   license "MIT"
 
