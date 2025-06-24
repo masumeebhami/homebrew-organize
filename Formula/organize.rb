a4be7053fba74bc6762417c4b4dcef196f5099bf
Masi <masumeebhami@gmail.com>
2025-06-24 11:56:18 +0200

    added sha256

--- Formula/organize.rb
+++ Formula/organize.rb
@@ -2,7 +2,7 @@ class Organize < Formula
   desc "Smart file organizer for macOS, written in Rust"
   homepage "https://github.com/masumeebhami/organize"
   url "https://github.com/masumeebhami/organize/releases/download/alpha/organize-macos.tar.gz"
-  sha256 "PASTE_YOUR_SHA_HERE"
+  sha256 "7573a1732ac648928a8fd20a2864b867ee4cf5aa26f275e29a70519035b0868d"
   license "MIT"
 
   def install
