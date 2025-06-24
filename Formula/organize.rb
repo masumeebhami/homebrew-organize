eb7131db2aa56b250a2b0109568f90e8504a8286
Masi <masumeebhami@gmail.com>
2025-06-24 16:15:46 +0200

    updated version

--- Formula/organize.rb
+++ Formula/organize.rb
@@ -1,9 +1,9 @@
 class Organize < Formula
   desc "Smart file organizer for macOS, written in Rust"
   homepage "https://github.com/masumeebhami/organize"
-  url "https://github.com/masumeebhami/organize/releases/download/aplha0.1.1/organize-macos.tar.gz"
-  sha256 "b4fff92aca5a6ce0241dee65d0568f62e675b2c67fde5e4f282af6be6bcff062"
-  version "aplha0.1.1"
+  url "https://github.com/masumeebhami/organize/releases/download/v0.1.1/organize-macos.tar.gz"
+  sha256 "156143165d394940392d23022059e9d200432b7309cb51d793f30726111115aa"
+  version "v0.1.1"
   license "MIT"
 
   def install
