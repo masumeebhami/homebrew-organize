8312d0e4e307fbefdb4004b913e6c9458ecae209
Masi <masumeebhami@gmail.com>
2025-06-24 12:10:50 +0200

    version added to file

--- Formula/organize.rb
+++ Formula/organize.rb
@@ -3,6 +3,7 @@ class Organize < Formula
   homepage "https://github.com/masumeebhami/organize"
   url "https://github.com/masumeebhami/organize/releases/download/alpha/organize-macos.tar.gz"
   sha256 "7573a1732ac648928a8fd20a2864b867ee4cf5aa26f275e29a70519035b0868d"
+  version "alpha"
   license "MIT"
 
   def install
